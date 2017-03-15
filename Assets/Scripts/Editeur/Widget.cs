f195b05ad1e2d6293b8006fa32ae8884a9741f5f
Ferre Arthur <arthur.ferre@etu.umontpellier.fr>
2017-03-15 12:53:30 +0100

    Restructuration de la class Widget, ajout des commentaires et commencement du widget Scrollbar

--- Assets/Scripts/Editeur/Widget.cs
+++ Assets/Scripts/Editeur/Widget.cs
@@ -2,26 +2,168 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Widget {
+namespace WarBotEngine.Editeur
+{
 
-    public Widget parent = null;
+    /// <summary>
+    /// Class abstraite dont les widgets héritent
+    /// </summary>
+    public abstract class Widget
+    {
 
-    public Rect area = new Rect(new Vector2(0, 0), new Vector2(0, 0));
 
-    public virtual void OnDestroy() { }
+        /***********************
+         ****** DELEGATES ******
+         ***********************/
 
-    public virtual void OnDraw() { }
 
-    public virtual void OnText() { }
+        /// <summary>
+        /// Delegate permettant de gérer les évènements des widgets
+        /// </summary>
+        /// <param name="args">paramètre quelconque de l'évènement</param>
+        public delegate void EventDelegate(object args);
 
-    public virtual void OnUpdate() { }
 
-    public virtual void OnKeyEvent(KeyCode keycode, bool state) { }
+        /***********************
+         ****** ATTRIBUTS ******
+         ***********************/
 
-    public virtual void OnMouseEvent(int button, bool pressed, int x, int y) { }
 
-    public virtual void OnMotionEvent(int x, int y) { }
+        protected Widget parent = null;
 
-    public virtual void OnScrollEvent(int delta) { }
+        protected Rect area = new Rect(new Vector2(0, 0), new Vector2(0, 0));
+
+        protected List<Widget> childs = new List<Widget>();
+
+        protected bool active = true;
+
+
+        /************************
+         ****** ACCESSEURS ******
+         ************************/
+
+
+        /// <summary>
+        /// Parent du widget s'il en a un et null sinon
+        /// </summary>
+        public Widget Parent { get { return parent; } set { parent = value; } }
+
+        /// <summary>
+        /// Coordonnées et dimensions du widget
+        /// </summary>
+        public Rect Area { get { return area; } }
+
+        /// <summary>
+        /// Widgets contenus dans le widget actuel
+        /// </summary>
+        public Widget[] Childs { get { return childs.ToArray(); } }
+
+        /// <summary>
+        /// Détermine si le widget est actif ou non
+        /// </summary>
+        public bool Active { get { return active; } set { active = value; } }
+
+
+        /***********************************
+         ****** METHODES D'EVENEMENTS ******
+         ***********************************/
+
+
+        /// <summary>
+        /// Appelée lors de la suppression du widget
+        /// </summary>
+        public virtual void OnDestroy()
+        {
+            foreach (Widget w in childs)
+            {
+                w.OnDestroy();
+            }
+        }
+
+        /// <summary>
+        /// Appelée lors du rendu de la scene avec les fonctions OpenGL
+        /// </summary>
+        public virtual void OnDrawWithGL() { }
+
+        /// <summary>
+        /// Appelée après le rendu de la scene avec les fonctions OpenGL
+        /// </summary>
+        public virtual void OnDrawWithoutGL() { }
+
+        /// <summary>
+        /// Appelée lors de la mise à jour des composants
+        /// </summary>
+        public virtual void OnUpdate() { }
+
+        /// <summary>
+        /// Appelée lors d'un évènement clavier
+        /// </summary>
+        /// <param name="keycode">touche associée à l'évènement</param>
+        /// <param name="state">indique si la touche est appuyée ou non</param>
+        public virtual void OnKeyEvent(KeyCode keycode, bool state) { }
+
+        /// <summary>
+        /// Appelée lors d'un évènement souris de bouton
+        /// </summary>
+        /// <param name="button">bouton associé à l'évènement</param>
+        /// <param name="pressed">indique si le bouton est appuyé ou non</param>
+        /// <param name="x">coordonnée en x de la souris</param>
+        /// <param name="y">coordonnée en y de la souris</param>
+        public virtual void OnMouseEvent(int button, bool pressed, int x, int y) { }
+
+        /// <summary>
+        /// Appelée lors d'un évènement souris de mouvement
+        /// </summary>
+        /// <param name="x">coordonnée en x de la souris</param>
+        /// <param name="y">coordonnée en y de la souris</param>
+        public virtual void OnMotionEvent(int x, int y) { }
+
+        /// <summary>
+        /// Appelée lors d'un évènement souris de scrolling
+        /// </summary>
+        /// <param name="delta">valeur de scrolling</param>
+        public virtual void OnScrollEvent(int delta) { }
+
+
+        /*********************************
+         ****** METHODES DE GESTION ******
+         *********************************/
+
+
+        /// <summary>
+        /// Ajoute un widget enfant au widget actuel
+        /// </summary>
+        /// <param name="widget">widget à ajouter</param>
+        public virtual void AddChild(Widget widget)
+        {
+            if (!childs.Contains(widget))
+            {
+                widget.Parent = this;
+                childs.Add(widget);
+            }
+        }
+
+        /// <summary>
+        /// Supprime un widget enfant du widget actuel
+        /// </summary>
+        /// <param name="widget">widget à supprimer</param>
+        public virtual void RemoveChild(Widget widget)
+        {
+            if (childs.Contains(widget))
+            {
+                widget.Parent = null;
+                childs.Remove(widget);
+            }
+        }
+
+        /// <summary>
+        /// Supprime tous les widgets enfants du widget actuel
+        /// </summary>
+        public virtual void RemoveAllChilds()
+        {
+            childs.Clear();
+        }
+
+    }
 
 }
--- Assets/Scripts/Editeur/Widget/SelectorTeam.cs
+++ Assets/Scripts/Editeur/Widget/SelectorTeam.cs
@@ -2,50 +2,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SelectorTeam : Widget {
+namespace WarBotEngine.Editeur
+{
 
-    private static readonly int dim_margin_top = 20;
-    private static readonly int dim_padding = 5;
-
-    private static readonly float dim_scale_width = 0.8f;
-    private static readonly int dim_pixel_height = 30;
-
-    private static readonly int dim_triangle_size = 20;
-
-    public SelectorTeam(SelectorPanel parent)
+    public class SelectorTeam : Widget
     {
-        this.parent = parent;
-        float width = parent.area.width * dim_scale_width;
-        this.area = new Rect((parent.area.width - width) / 2, dim_margin_top, width, dim_pixel_height);
-    }
 
-    public override void OnDraw()
-    {
-        float left = (float)this.area.x / (float)Screen.width, right = (float)this.area.xMax / (float)Screen.width, bottom = (float)(Screen.height - this.area.yMin) / (float)Screen.height, top = (float)(Screen.height - this.area.yMax) / (float)Screen.height;
-        GL.Begin(GL.QUADS);
-        GL.Color(Color.white);
-        GL.Vertex3(left, top, 0);
-        GL.Vertex3(right, top, 0);
-        GL.Vertex3(right, bottom, 0);
-        GL.Vertex3(left, bottom, 0);
-        GL.End();
-        left = (float)(this.area.xMax - dim_padding - dim_triangle_size) / (float)Screen.width;
-        right = (float)(this.area.xMax - dim_padding) / (float)Screen.width;
-        bottom = (float)(Screen.height - this.area.yMax + dim_padding) / (float)Screen.height;
-        top = (float)(Screen.height - this.area.yMax + dim_padding + dim_triangle_size) / (float)Screen.height;
-        GL.Begin(GL.TRIANGLES);
-        GL.Color(Color.black);
-        GL.Vertex3(left, top, 0);
-        GL.Vertex3((left + right) / 2, bottom, 0);
-        GL.Vertex3(right, top, 0);
-        GL.End();
-    }
+        private static readonly int dim_margin_top = 20;
+        private static readonly int dim_padding = 5;
 
-    public override void OnText()
-    {
-        Rect rect = new Rect(this.area.x + dim_padding, this.area.y + dim_padding, this.area.width, this.area.height - dim_padding * 2);
-        GUI.color = Color.black;
-        GUI.Label(rect, "EQUIPE");
+        private static readonly float dim_scale_width = 0.8f;
+        private static readonly int dim_pixel_height = 30;
+
+        private static readonly int dim_triangle_size = 20;
+
+        public SelectorTeam(SelectorPanel parent)
+        {
+            this.parent = parent;
+            float width = parent.Area.width * dim_scale_width;
+            this.area = new Rect((parent.Area.width - width) / 2, dim_margin_top, width, dim_pixel_height);
+        }
+
+        public override void OnDrawWithGL()
+        {
+            float left = (float)this.area.x / (float)Screen.width, right = (float)this.area.xMax / (float)Screen.width, bottom = (float)(Screen.height - this.area.yMin) / (float)Screen.height, top = (float)(Screen.height - this.area.yMax) / (float)Screen.height;
+            GL.Begin(GL.QUADS);
+            GL.Color(Color.white);
+            GL.Vertex3(left, top, 0);
+            GL.Vertex3(right, top, 0);
+            GL.Vertex3(right, bottom, 0);
+            GL.Vertex3(left, bottom, 0);
+            GL.End();
+            left = (float)(this.area.xMax - dim_padding - dim_triangle_size) / (float)Screen.width;
+            right = (float)(this.area.xMax - dim_padding) / (float)Screen.width;
+            bottom = (float)(Screen.height - this.area.yMax + dim_padding) / (float)Screen.height;
+            top = (float)(Screen.height - this.area.yMax + dim_padding + dim_triangle_size) / (float)Screen.height;
+            GL.Begin(GL.TRIANGLES);
+            GL.Color(Color.black);
+            GL.Vertex3(left, top, 0);
+            GL.Vertex3((left + right) / 2, bottom, 0);
+            GL.Vertex3(right, top, 0);
+            GL.End();
+        }
+
+        public override void OnDrawWithoutGL()
+        {
+            Rect rect = new Rect(this.area.x + dim_padding, this.area.y + dim_padding, this.area.width, this.area.height - dim_padding * 2);
+            GUI.color = Color.black;
+            GUI.Label(rect, "EQUIPE");
+        }
+
     }
 
 }
