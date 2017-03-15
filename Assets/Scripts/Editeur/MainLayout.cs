e5ecf2ca444ad87dc8677702efdc1f7e466a5719
Ferre Arthur <arthur.ferre@etu.umontpellier.fr>
2017-03-15 12:53:30 +0100

    Restructuration de la class Widget, ajout des commentaires et commencement du widget Scrollbar

--- Assets/Scripts/Editeur/MainLayout.cs
+++ Assets/Scripts/Editeur/MainLayout.cs
@@ -4,160 +4,268 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class MainLayout : MonoBehaviour
+namespace WarBotEngine.Editeur
 {
 
-    private static MainLayout actual;
-
-    public static MainLayout Actual { get { return actual; } }
-
-    private List<Widget> widgets;
-
-    public GameObject MainCanvas;
-
-    private Vector2 mousePos;
-
-    // Use this for initialization
-    void Start()
+    /// <summary>
+    /// Gestionnaire des widgets à l'écran
+    /// </summary>
+    public class MainLayout : MonoBehaviour
     {
-        MainLayout.actual = this;
-        widgets = new List<Widget>();
-        mousePos = new Vector2();
 
-        this.AddWidget(new SelectorPanel());
-        this.AddWidget(new Box(new Rect(700, 300, 100, 100), "Primitive 2", true, null));
-        this.AddWidget(new Box(new Rect(500, 200, 100, 100), "Primitive 1", false, (Box)widgets[widgets.Count - 1]));
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        OnUpdate();
-    }
+        /***********************
+         ****** ATTRIBUTS ******
+         ***********************/
 
-    public void AddWidget(Widget obj)
-    {
-        widgets.Add(obj);
-    }
 
-    public void RemoveWidget(Widget obj)
-    {
-        obj.OnDestroy();
-        widgets.Remove(obj);
-    }
+        private static MainLayout actual;
 
-    static Material lineMaterial;
-    static void CreateLineMaterial()
-    {
-        if (!lineMaterial)
+        private List<Widget> widgets;
+
+        private Vector2 mouse_position;
+
+        private static Material lineMaterial;
+
+
+        /****************************
+         ****** OBJETS D'UNITY ******
+         ****************************/
+
+
+        public GameObject MainCanvas;
+
+
+        /************************
+         ****** ACCESSEURS ******
+         ************************/
+
+
+        /// <summary>
+        /// Layout actuel
+        /// </summary>
+        public static MainLayout Actual { get { return actual; } }
+
+        /// <summary>
+        /// Liste des widgets
+        /// </summary>
+        public Widget[] Widgets { get { return widgets.ToArray(); } }
+
+
+        /****************************
+         ****** APPELS D'UNITY ******
+         ****************************/
+
+
+        /// <summary>
+        /// Appelée à l'initialisation du GameObject
+        /// </summary>
+        void Start()
         {
-            // Unity has a built-in shader that is useful for drawing
-            // simple colored things.
-            Shader shader = Shader.Find("Hidden/Internal-Colored");
-            lineMaterial = new Material(shader);
-            lineMaterial.hideFlags = HideFlags.HideAndDontSave;
-            // Turn on alpha blending
-            lineMaterial.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
-            lineMaterial.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
-            // Turn backface culling off
-            lineMaterial.SetInt("_Cull", (int)UnityEngine.Rendering.CullMode.Off);
-            // Turn off depth writes
-            lineMaterial.SetInt("_ZWrite", 0);
+            MainLayout.actual = this;
+            widgets = new List<Widget>();
+            mouse_position = new Vector2();
+
+            this.AddWidget(new SelectorPanel());
+            this.AddWidget(new Box(new Rect(700, 300, 100, 100), "Primitive 2", true, null));
+            this.AddWidget(new Box(new Rect(500, 200, 100, 100), "Primitive 1", false, (Box)widgets[widgets.Count - 1]));
         }
-    }
 
-    public void OnDraw()
-    {
-        CreateLineMaterial();
-        lineMaterial.SetPass(0);
-        GL.PushMatrix();
-        GL.LoadOrtho();
-        foreach (Widget o in widgets)
+        /// <summary>
+        /// Appelée à l'update du GameObject (chaque frame)
+        /// </summary>
+        void Update()
         {
-            o.OnDraw();
+            OnUpdate();
         }
-        GL.PopMatrix();
-        foreach (Widget o in widgets)
-        {
-            o.OnText();
-        }
-    }
 
-    public void OnUpdate()
-    {
-        foreach (Widget o in widgets)
+        /// <summary>
+        /// Appelée à chaque évènement
+        /// </summary>
+        void OnGUI()
         {
-            o.OnUpdate();
-        }
-    }
-
-    public void OnKeyEvent(KeyCode keycode, bool state)
-    {
-        print("[Key]: " + keycode + " = " + state);
-        foreach (Widget o in widgets)
-        {
-            o.OnKeyEvent(keycode, state);
-        }
-    }
-
-    public void OnMouseEvent(int button, bool pressed, int x, int y)
-    {
-        print("[Mouse]: " + button + " = " + pressed + " -> " + x + " , " + y);
-        foreach (Widget o in widgets)
-        {
-            o.OnMouseEvent(button, pressed, x, y);
-        }
-    }
-
-    public void OnMotionEvent(int x, int y)
-    {
-        print("[Motion]: " + x + " , " + y);
-        foreach (Widget o in widgets)
-        {
-            o.OnMotionEvent(x, y);
-        }
-    }
-
-    public void OnScrollEvent(int delta)
-    {
-        print("[Scroll]: " + delta);
-        foreach (Widget o in widgets)
-        {
-            o.OnScrollEvent(delta);
-        }
-    }
-
-    public void OnGUI()
-    {
-        Event e = Event.current;
-        if (mousePos != e.mousePosition)
-        {
-            mousePos = e.mousePosition;
-            OnMotionEvent((int)e.mousePosition.x, (int)e.mousePosition.y);
-        }
-        switch (e.type)
-        {
-            case EventType.MouseDown:
-                OnMouseEvent(e.button, true, (int)e.mousePosition.x, (int)e.mousePosition.y);
-                break;
-            case EventType.MouseUp:
-                OnMouseEvent(e.button, false, (int)e.mousePosition.x, (int)e.mousePosition.y);
-                break;
-            case EventType.MouseMove:
+            Event e = Event.current;
+            if (mouse_position != e.mousePosition)
+            {
+                mouse_position = e.mousePosition;
                 OnMotionEvent((int)e.mousePosition.x, (int)e.mousePosition.y);
-                break;
-            case EventType.ScrollWheel:
-                OnScrollEvent((int)e.delta.y);
-                break;
-            case EventType.KeyDown:
-                OnKeyEvent(e.keyCode, true);
-                break;
-            case EventType.KeyUp:
-                OnKeyEvent(e.keyCode, false);
-                break;
-            case EventType.Repaint:
-                OnDraw();
-                break;
+            }
+            switch (e.type)
+            {
+                case EventType.MouseDown:
+                    OnMouseEvent(e.button, true, (int)e.mousePosition.x, (int)e.mousePosition.y);
+                    break;
+                case EventType.MouseUp:
+                    OnMouseEvent(e.button, false, (int)e.mousePosition.x, (int)e.mousePosition.y);
+                    break;
+                case EventType.MouseMove:
+                    OnMotionEvent((int)e.mousePosition.x, (int)e.mousePosition.y);
+                    break;
+                case EventType.ScrollWheel:
+                    OnScrollEvent((int)e.delta.y);
+                    break;
+                case EventType.KeyDown:
+                    OnKeyEvent(e.keyCode, true);
+                    break;
+                case EventType.KeyUp:
+                    OnKeyEvent(e.keyCode, false);
+                    break;
+                case EventType.Repaint:
+                    OnDraw();
+                    break;
+            }
+        }
+
+
+        /*********************************
+         ****** METHODES DE GESTION ******
+         *********************************/
+
+
+        /// <summary>
+        /// Ajoute un widget à la liste
+        /// </summary>
+        /// <param name="obj"></param>
+        public void AddWidget(Widget obj)
+        {
+            widgets.Add(obj);
+        }
+
+        /// <summary>
+        /// Supprime un widget de la liste
+        /// </summary>
+        /// <param name="obj"></param>
+        public void RemoveWidget(Widget obj)
+        {
+            widgets.Remove(obj);
+        }
+
+        /// <summary>
+        /// Supprime tous les widgets de la liste
+        /// </summary>
+        public void Clear()
+        {
+            foreach (Widget o in widgets)
+            {
+                o.OnDestroy();
+            }
+        }
+
+        /// <summary>
+        /// Crée un matériau pour l'affichage avec les fonctions d'OpenGL
+        /// </summary>
+        private static void CreateLineMaterial()
+        {
+            if (!lineMaterial)
+            {
+                // Unity has a built-in shader that is useful for drawing
+                // simple colored things.
+                Shader shader = Shader.Find("Hidden/Internal-Colored");
+                lineMaterial = new Material(shader);
+                lineMaterial.hideFlags = HideFlags.HideAndDontSave;
+                // Turn on alpha blending
+                lineMaterial.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+                lineMaterial.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+                // Turn backface culling off
+                lineMaterial.SetInt("_Cull", (int)UnityEngine.Rendering.CullMode.Off);
+                // Turn off depth writes
+                lineMaterial.SetInt("_ZWrite", 0);
+            }
+        }
+
+
+        /***********************************
+         ****** METHODES D'EVENEMENTS ******
+         ***********************************/
+
+
+        /// <summary>
+        /// Appelée lors de l'affichage
+        /// </summary>
+        public void OnDraw()
+        {
+            CreateLineMaterial();
+            lineMaterial.SetPass(0);
+            GL.PushMatrix();
+            GL.LoadOrtho();
+            foreach (Widget o in widgets)
+            {
+                o.OnDrawWithGL();
+            }
+            GL.PopMatrix();
+            foreach (Widget o in widgets)
+            {
+                o.OnDrawWithoutGL();
+            }
+        }
+
+        /// <summary>
+        /// Appelée lors de l'update
+        /// </summary>
+        public void OnUpdate()
+        {
+            foreach (Widget o in widgets)
+            {
+                o.OnUpdate();
+            }
+        }
+
+        /// <summary>
+        /// Appelée lors d'un évènement clavier
+        /// </summary>
+        /// <param name="keycode">touche associée à l'évènement</param>
+        /// <param name="state">indique si la touche est appuyée ou non</param>
+        public void OnKeyEvent(KeyCode keycode, bool state)
+        {
+            print("[Key]: " + keycode + " = " + state);
+            foreach (Widget o in widgets)
+            {
+                o.OnKeyEvent(keycode, state);
+            }
+        }
+
+        /// <summary>
+        /// Appelée lors d'un évènement souris de bouton
+        /// </summary>
+        /// <param name="button">bouton associé à l'évènement</param>
+        /// <param name="pressed">indique si le bouton est appuyé ou non</param>
+        /// <param name="x">coordonnée en x de la souris</param>
+        /// <param name="y">coordonnée en y de la souris</param>
+        public void OnMouseEvent(int button, bool pressed, int x, int y)
+        {
+            print("[Mouse]: " + button + " = " + pressed + " -> " + x + " , " + y);
+            foreach (Widget o in widgets)
+            {
+                o.OnMouseEvent(button, pressed, x, y);
+            }
+        }
+
+        /// <summary>
+        /// Appelée lors d'un évènement souris de mouvement
+        /// </summary>
+        /// <param name="x">coordonnée en x de la souris</param>
+        /// <param name="y">coordonnée en y de la souris</param>
+        public void OnMotionEvent(int x, int y)
+        {
+            print("[Motion]: " + x + " , " + y);
+            foreach (Widget o in widgets)
+            {
+                o.OnMotionEvent(x, y);
+            }
+        }
+
+        /// <summary>
+        /// Appelée lors d'un évènement souris de scrolling
+        /// </summary>
+        /// <param name="delta">valeur de scrolling</param>
+        public void OnScrollEvent(int delta)
+        {
+            print("[Scroll]: " + delta);
+            foreach (Widget o in widgets)
+            {
+                o.OnScrollEvent(delta);
+            }
         }
     }
+
 }
---
+++ Assets/Scripts/Editeur/Widget/Scrollbar.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WarBotEngine.Editeur
+{
+
+    /// <summary>
+    /// Scollbar verticale
+    /// </summary>
+    public class Scrollbar : Widget
+    {
+
+        /***********************
+         ****** ATTRIBUTS ******
+         ***********************/
+
+
+        private float scroll_height;
+
+        private float actual_value = 0;
+
+        private event EventDelegate on_changevalue;
+
+
+        /************************
+         ****** ACCESSEURS ******
+         ************************/
+
+
+        /// <summary>
+        /// Hauteur totale du conteneur
+        /// </summary>
+        public float ScrollHeight
+        {
+            get
+            {
+                return scroll_height;
+            }
+            set
+            {
+                if (value < 0)
+                    scroll_height = 0;
+                else
+                    scroll_height = value;
+                if (scroll_height - parent.Area.height < actual_value)
+                    actual_value = scroll_height - parent.Area.height;
+                if (actual_value < 0)
+                    actual_value = 0;
+            }
+        }
+
+        /// <summary>
+        /// Position verticale actuelle
+        /// </summary>
+        public float CurrentValue
+        {
+            get
+            {
+                return actual_value;
+            }
+            set
+            {
+                actual_value = value;
+                if (scroll_height - parent.Area.height < actual_value)
+                    actual_value = scroll_height - parent.Area.height;
+                if (actual_value < 0)
+                    actual_value = 0;
+            }
+        }
+
+
+        /********************************************
+         ****** METHODES SPECIFIQUES AU WIDGET ******
+         ********************************************/
+
+
+        /// <summary>
+        /// Constructeur de base de la scrollbar
+        /// </summary>
+        /// <param name="area">coordonnées et dimensions</param>
+        /// <param name="scroll_height">hauteur totale du conteneur</param>
+        /// <param name="on_changevalue">évènement provoqué d'un changement de scrolling</param>
+        public Scrollbar(Rect area, float scroll_height, EventDelegate on_changevalue)
+        {
+            this.area = area;
+            this.scroll_height = scroll_height;
+            this.on_changevalue = on_changevalue;
+        }
+
+
+        /***********************************
+         ****** METHODES D'EVENEMENTS ******
+         ***********************************/
+
+
+        public override void OnDrawWithGL()
+        {
+            
+        }
+
+        public override void OnMouseEvent(int button, bool pressed, int x, int y)
+        {
+            
+        }
+
+        public override void OnMotionEvent(int x, int y)
+        {
+            
+        }
+
+        public override void OnScrollEvent(int delta)
+        {
+            
+        }
+
+    }
+
+}
